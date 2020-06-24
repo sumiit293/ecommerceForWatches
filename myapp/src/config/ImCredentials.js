202923afa813ef937ec6c74fab7c231df920a7a2
sumiit293 <53748800+sumiit293@users.noreply.github.com>
2020-06-24 08:52:13 +0530

    removing the banking credentials

--- myapp/src/config/ImCredentials.js
+++ myapp/src/config/ImCredentials.js
@@ -1,5 +1,5 @@
 export default {
-    "X-Auth-Key": "9d25ead2b811ff0361b7fe05b501fe54",
-    "X-Auth-Token": "5975cb9dc873581e9082adaaeb069e81",
-    "X-Auth-Salt": "13609d728e914551b50df1474b4de2cb"
+    "X-Auth-Key": "Your Auth Key",
+    "X-Auth-Token": "Your Instamojo Auth Token",
+    
 }
