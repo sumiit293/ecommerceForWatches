45a31235ac28f07218a9cb6fa28c78517c43e5c0
sumiit293 <53748800+sumiit293@users.noreply.github.com>
2020-06-24 08:49:59 +0530

    removing db credentials

--- config/MyUrl.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    connectionString: "mongodb+srv://Sumit_293:12345@cluster0-akjax.mongodb.net/test?retryWrites=true&w=majority",
-    secrect: "mystrongsecret"
-}
-// thesr are sensetve data
